52c062c3bdf9dd42ece191e41ec16eb0d9b827bc
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-03 04:22:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   if array.include?(cheese_types) == true
-    return cheese_types[i]
+  cheese_types[i]
     
   end
 end
